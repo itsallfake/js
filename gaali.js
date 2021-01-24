f2743ea965afee3cee66f5be929351f700b8ac09
Aakash Sharma <77550643+itsallfake@users.noreply.github.com>
2021-01-24 20:55:36 +0530

    Add files via upload

---
+++ gaali.js
@@ -0,0 +1,36 @@
+/* Coded By : @Headshot420 
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'gaali', fromMe: true}, (async (message, match) => {
+
+    var r_text = new Array ();
+r_text[0] = "Haan beta? baap se panga lega, teri maa ki. hut hijde , bhadwe kutiya ke bache.";
+r_text[1] = "Nikal na madarchod bhosdike gaand marwane aaya hai kya baap ke paas ya apni maa ki bur phadwayega madarchod, angrezo ki galti hai tu aur tera baap...pata nahi kaise apni maa chudwa lete ho angrezo se, mahatama gandhi ka takla le lete gand me bhenchodo!";
+r_text[2] = "Randi ke bache teri maa ki choot behn ke lode harami raand ki paidaish, teir bua ki chut me haathi ka lund behnchod teri dadi randi bhsodike, nalle baap ki aulad madarchod namard behn ke lode tatte ke saudagar gandu teri maa ki chut hijde ke bache!";
+r_text[3] = "Saale madarchod behn ke lode teri maa ki gand me johny sins ka lauda behnchod randi ki aulad madarchod chinnar ke bache hijde randi ke pille!";
+r_text[4] = "Road pe Road , road pe padi battery, tera baap chodega teri maa ko raat me jala ke battery,.randi ki gand fategi, saari duniya hasegi, teri maa chudegi teri behn choosegi, tera baap muh me lega teri dadi gand marwaegi, bhaa maadarchod behn ke lode";
+r_text[5] = "Main roz teri behno ki banjar chut me apna lawda daalke andar haryali lata tha magar aaj unke ke baare me sunke mujhe bhut afsos huwa ki unko ab bada loudha chahye ab mera balatkaaari lawda lagataar 4 ghante tk apne muh me kon rakhega vo teri behne hi thi jo apni kaali magar rasilli chut mere saamne khol deti aur zameen pe naagin ki tarah rengne lgti thi jaise ki kisine unki chut pe naariyal tod diya ho vo b bada wala mumbai ka naariyal..apni chennal maa ko b nhi bhej rahe mere paas to main kaixe tum logo se vaada karu ki main teri maa chodd dungaw ab agar tun sach me chahta hai ki main tum dono k mc ki chut me dhammal karu to mera lawda apne muh me rakho aur kaho sidharth hamare sage papa hain Aur agar tb b tjhe apni maa ki kaali chut mere saamne nahi rakhi to tumhare ghar me ghuske tumhari maa ka balatkaar kar dungaw jaixe delhi me huwa tha ab teri chudi hui kuttiyo ki tarah apni gaand hilaate hue mere aage kalapna mt ni to tumhari fatti bhoxdi me 100 ched karunga!";
+r_text[6] = "Madarchod bhadwe chutiya aadmi gaand fati aulad teri ma ki chut me 4 kilo besan bhosdike ghacke maarke pakode niklenge behn ke laude randi aurat! Teri maa ki gand me power ranger ka lauda behnchod SPD walw chodenge teri ma ko ghodi bana bana ke madarchod bhadwe!";
+r_text[7] = "Chakke ki aulad teri maa chud rahi thi kya behn ke lode , bandar ke Tatte chaat ke bada hua hai kya? behn ke lode teri aukat me nahi hai jeena, tu lauda pakad ke naach bas behnchod 2 rupay ki randi gand marwake guzara karne wali teri behn ki choot maaru bhag madarchod!";
+r_text[8] = "Teri behn ki chut me thanos ka haath bhsodike, iron man ki rakhail ke bete teri behn chodi, teri dadi chodi, teri ma ki chut chodi bhosdike, teri ma ki chut 10 baar hijde randi, teri ma ki choot me kela deke ghacke maaru madarchod hijde saale teri maa ki burchat bhosdike rand ki aulad.";
+r_text[9] = "Teri maa ki chut maare hathi, ghode, kutte behn ke lode , teri gand me mirch dekhe maaro tere gand pe laat behnchod gandve bina laude ke mard , chakke teri behn ki chut me magnesium deke maaru madarchod behn ke lode teri maa chude saale kothe pe naachke teri parwarish akr rahi behnchod!";
+r_text[10] = "Gandve chutiya lodu Bhn ke lode maadhrchod Teri maa ki chut bhosdk bhn k dine Teri maa ka bhosda, tbuchate aadki teri behn ki chut me saande ka lund behnchod sing maar maar ke teri maa ki chut faad denge bhadwe randi ki aulad behnchod teri gand me india gate ka khamba deke chodi kar do behnchod bhadwe teri behn ke bhsode me aag laga ke jala do aur teri maa ki bur me kutte ke lund dede maardo raand ki bachi ko! ";
+r_text[11] = "Teri behn ki chut maare scooby-doo bhosdike, kaaliya se apni maa chudwa aur chota bheem teri daadi ki chut fadega bhosdike madarchod insaan, teri maa ki chut me sande ka tel laga ke ghode ka lund visarjit kardu hijde!";
+r_text[12] = "Madarchod Randi ke bacche Oye bosdike madarchod bhen ke lode tere gand me lohe ka danda garam karke dalu randwe tujhetho gali ke kutte gand pe chut rakh ke katenge me bata raha hu tere lode pe madhu makkhi Katelode ke ando pe Road roller chale tu kab bathroom me muthne Jaye tho Tera loda ghir Jaye fir tere ando me se lizard ke bacche nikle teko kidnap Kare aur childporn banaye maa ke chuttad ke lode tere saat Johnny sins rape Kare aur jab wo teko anal de tab loda andar fas Jaye bkl tere jhaat pe waxing karunga me dhek lio fir jab tu chillayega na tab tere muh me Mai gai ka gobar dalunga sale tere gand ke balo pe tel laga ke jala du me teko Anaconda leke gand me dalu tho muh se nikle maa ke lode hamesha chutiyo jaisa bartav kartha he tu maa ke Dai chawal drugs tere gand Me dalunga thi tatti nahi nikle maa darchod kabhi teko Marne ka mouka mil gaya na tho bas I'll do my best to get that tatti outof you aur tere jaise chutio ko is duniya me jagaha bhi nahi maa ke lode bandarchod tere gand me chitiya Kate wo bhi bullet ants maadarchod samj nahi aaraha tere baap NE teko kya khake paida kiya Tha kesa chutiya he tu rand ke bacche teko shadi me khana khane na mile teko gand pe 4 thappad mare sab log aur blade se likhe I want anal madarchod bosdike maccharki tatte ke baal chutiye maa ke chut pe ghode ka Lund tere gand me jaltha hu koila Dale bhen ke lode MAA KI CHUT MAI TALWAR DUNGA BC CHUT FAT JAEGI AUR USME SE ITNA KHOON NIKLEGA MZA AJAEGA DEKHNE KA SALE MAA KE BHOSDE SE BAHR AJA FIR BAAP SE ZUBAN DA TERI MAA KI CHUT CHOD CHOD KE BHOSDABNADU MADARCHOD AUR USKE UPAR CENENT LAGADU KI TERE JESA GANDU INSAAN KABHI BAHR NA A SKE ESI GANDI CHUT MAI SE LODA LASUN MADRCHOD TERI MAA KI CHUT GASTI AMA KA CHUTIA BACHA TERI MAA KO CHOD CHOD K PAGAL KAR DUNGA MAA K LODY KISI SASTIII RANDII K BACHY TERI MAA KI CHOOT MAIN TEER MAARUN GANDU HARAMI TERI COLLEGE JATI BAJI KA ROAD PEY RAPE KARONGANDU KI OLAAD HARAM KI NASAL PAPA HUN TERA BHEN PESH KAR AB PAPA KO TERI MAA KKALE KUSS MAIN KISI!";
+r_text[13] = "Chaar chawanni ghode pe, Teri maa mere lawde pe, Behnchod nazayaz aulad teri gand ke saare keede nikal jayenge ek baar apne baap ka muh me leke dekh, bhosdi raand ke pille gaand fatti randi ki paidaish.";
+r_text[14] = "Abe oo Fate condom ki aulad, teri maa ki bhi itni choot nahi maari hogi kisi ne jitna merse marwa rha hai, bhosdike gaand me sutli bomb laga ke soja...5 minute me gaand ka gudgama bana payega. bhaag madarchod hijde.";
+r_text[15] = "Chal Teri maa ki chut behn ke lode, teri behn ki fuddi me hara dhaniya maaru bhosdike!";
+var i = Math.floor(16*Math.random())
+
+await message.sendMessage(r_text[i]);
+
+}));
+
+Asena.addCommand({pattern: 'infogaali', fromMe: true}, (async (message, match) => {
+
+    await message.sendMessage("*Codded by @Headshot420*\n��Usage: *.gaali*\nDesc: Send random gaali to anyone! #only_for_fun")
+
+}));
